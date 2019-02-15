8517a4c5b4df3ac1104f613cfbd118db4ff536ae
Chaplaso-Ola <chaplaso@yahoo.com>
2019-02-14 22:32:51 -0500

    WIP

--- models.js
+++ models.js
@@ -12,11 +12,22 @@ const Gardner = conn.define("gardener", {
     age: Sequelize.INTEGER
 })
 
+const Plot = conn.define('plot', {
+    size: Sequelize.STRING,
+    shaded:Sequelize.BOOLEAN
+})
+
 const Vegetable = conn.define('vegetable', {
     name:Sequelize.STRING,
     color:Sequelize.STRING,
     planted_on: Sequelize.DATE
 })
+
+Plot.belongsTo(Gardner)
+Gardner.hasOne(Plot)
+Vegetable.belongsToMany(Plot,{through: 'vegetable_plot'})
+Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
+Gardner.belongsTo(Vegetable, {as: 'favorite_vegetable'})
 
 
 
